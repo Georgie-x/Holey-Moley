d603b6d30c034bf078738ffd04696e4770be0603
Georgie-x <Georgiex@gmail.com>
2023-11-06 09:51:18 +0000

    CSS cleared, layout components added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  return <>
+  <Header />
+  <Nav />
+  <Sockets />
+  <SignIn />
+  <Auth />
+  <PageDisplay />
+  </>;
+}
+
+export default App;
