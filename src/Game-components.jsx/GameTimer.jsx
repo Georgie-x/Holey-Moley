6d022c8c4587ccf1d0a4b5e1b8bbcb00a2bac2b7
Maisie McGhee <maisiemcghee@tests-MacBook-Pro.local>
2023-11-10 15:55:10 +0000

    loop through celebs

--- src/Game-components.jsx/GameTimer.jsx
+++ src/Game-components.jsx/GameTimer.jsx
@@ -6,25 +6,27 @@ const socket = io.connect("http://localhost:3001")
 function GameTimer({celebURLs}) {
     const [index, setIndex] = useState(0)
     const [celebURL, setCelebURL] = useState(celebURLs[0])
-    console.log(celebURLs[0])
-    const [timer, setTimer] = useState(30)
+    const [timer, setTimer] = useState(10)
 
     useEffect(() => {
+        if(!timer) {
+        socket.on(`update_celeb`, (newCelebURL) => {
+            setCelebURL(newCelebURL)
+            console.log(index)
+        }) 
+        }
         socket.on(`update_timer`, (countdownDuration) => {
             setTimer(countdownDuration)
             if(countdownDuration === 0) {
                 handleNextCeleb()
+                setIndex(index+1)
             }
-        })
-        socket.on(`update_celeb`, (newCelebURL) => {
-            setCelebURL(newCelebURL)
-            setIndex(index + 1)
         })
         return () => {
             socket.off(`update_timer`)
             socket.off(`update_celeb`)
         }
-    }, [])
+    }, [timer])
 
     const startTimer = () => {
         if (socket) {
@@ -39,23 +41,38 @@ function GameTimer({celebURLs}) {
     }
 
     const handleNextCeleb = () => {
-        const randomId = Math.floor(celebURLs.length * Math.random())
-        console.log(randomId)
-        const newCelebURL = celebURLs[randomId]
+        const newCelebURL = celebURLs[index]
         socket.emit("new_game", newCelebURL);
+        setIndex((index+1)%celebURLs.length)
         setCelebURL(newCelebURL);
         resetTimer();
         startTimer();
     }
+        
+        // console.log(randomId)
+        // const newCelebURL = celebURLs[(index)];
+        // socket.emit("new_game", newCelebURL);
+        // setIndex((index+1));
+        // setCelebURL(newCelebURL);
+        // resetTimer();
+        // startTimer();
 
-    return (
-        <>
-        {/* <button className='next-celeb-button' onClick={handleNextCeleb}>Next Celeb</button> */}
-  <p className='time-remaining'>{timer} seconds remaining</p>
-        <div className="timer">
-            <img className="celeb-picture" src={celebURL} alt="a picture of a random celebrity" />
-        </div>
-        </>
-    )
+        return (
+            <>
+              {index >= 5 ? (
+                <div className="game-over">
+                  <h1>Game Over</h1>
+                </div>
+              ) : (
+                <>
+                 <button className='next-celeb-button' onClick={handleNextCeleb}>Next Celeb</button>
+                  <p className='time-remaining'>{timer} seconds remaining</p>
+                  <div className="timer">
+                    <img className="celeb-picture" src={celebURL} alt="a picture of a random celebrity" />
+                  </div>
+                </>
+              )}
+            </>
+          );
 }
 export default GameTimer
